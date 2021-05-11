437bc18b4925ecf28e37074fc001eb6585fad0de
DeFailingCode <83812998+DeFailingCode@users.noreply.github.com>
2021-05-11 10:00:29 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
-function myFunction() {
-	
+function B1() {
+}
+function B2() {
+}
+function B3() {
+}
+function B4() {
+}
+function B5() {
+}
+function B6() {
+}
+function B7() {
+}
+function B8() {
+}
+function B9() {
+}
+function B0() {
+}
+function BC() {
 }
